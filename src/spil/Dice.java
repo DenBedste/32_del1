a8c11a50a275b7bea8069884970696e66aeaed07
TahanyNemer1 <32563912+TahanyNemer1@users.noreply.github.com>
2017-10-06 23:09:06 +0200

    Update Dice.java

--- src/spil/Dice.java
+++ src/spil/Dice.java
@@ -1,20 +1,21 @@
 package spil;
 
-public class terning {
+public class Terning {
  
- //	default constructer
-	public Terninger() {
-		roll();
- }
- 
-  //konstruktør
-public terning(int diceValue) {
-  }
- public int kast() {
-  float d1=(float)Math.random();
-  float d2=d1*5;
-  int d3 = Math.round(d2);
-  return d3 + 1;
- }
- public int 
+	
+ private int roll() {
+	 float d1 = (float)Math.random();
+	 float d2 = d1 * 5;
+	 int d3 = Math.round(d2); 
+	 return d3 + 1; 
+	
+	public int rollMultiple(int n) {
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			result = roll();
+		}
+		return result;
+	}
 }
+	
+
