77378a0dea8788a1c275b17ed24294ec648a397c
TahanyNemer1 <32563912+TahanyNemer1@users.noreply.github.com>
2017-10-08 11:03:03 +0200

    Update player.java

--- src/spil/player.java
+++ src/spil/player.java
@@ -19,4 +19,16 @@ public int getPoint(){
 return point;
 	}
 	
+void rollDice(int n) {
+int f1 = 0,f2 = 0;
+	
+f1 = d1.rollMultiple(n);
+f2 = d2.rollMultiple(n);
+	
+point += f1 + f2;
+	
+System.out.printf(" you got %d and %d wich equals %d" ,f1, f2, f1+f2);
+System.out.printf(" your new account is %d\n " , point);
+}
+	
 	
