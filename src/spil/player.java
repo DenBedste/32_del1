5d797bead0760e26ce29b0199d32845e37580470
TahanyNemer1 <32563912+TahanyNemer1@users.noreply.github.com>
2017-10-08 10:57:32 +0200

    Update player.java

--- src/spil/player.java
+++ src/spil/player.java
@@ -1,12 +1,14 @@
 public class Dice_spillere{
 
 private String name;
-	private int point;
-  private Terninger d1,d2;
+private int point;
+private Terninger d1,d2;
   
   Dice_spillere(String name){
-		this.name = name;
-    point = 0;
-    d1 = new Terninger();
-		d2 = new Terninger();
+this.name = name;
+point = 0;
+d1 = new Terninger();
+d2 = new Terninger();
 }
+	
+	
