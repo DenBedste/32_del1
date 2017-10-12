ba04a8a957d80b2400b1667ed743a050366db5a5
denzildk <32563613+denzildk@users.noreply.github.com>
2017-10-12 12:43:56 +0200

    dice_fix
    tilføjet return til dice

--- src/spil/Terning.java
+++ src/spil/Terning.java
@@ -1,8 +1,9 @@
 package spil;
 public class Terning {
  private int roll() {
-	int d1 = (int) (math.random()*6) + 1;
-	
+	int d1 = (int) (Math.random()*6) + 1;
+	return d1;
+ }
 	public int rollMultiple(int n) {
 		int result = 0;
 		for (int i = 0; i < n; i++) {
