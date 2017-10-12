c3ab7dece3462561c33c2f98d99a86b1a04e3d87
denzildk <hedegaard.cornelius@gmail.com>
2017-10-12 16:57:24 +0200

    cleanup bæger

--- src/spil/Bæger.java
+++ src/spil/Bæger.java
@@ -1,21 +1,16 @@
 package spil;
 
 public class Bæger {
-<<<<<<< HEAD
-	public Bæger() {
-	Terning t1 = new Terning();
-	Terning t2 = new Terning();
-		
-		
-		
-	}
-	public int bæger(String Bruger) {
-		return t1+t2;
-		
-		
-		
-	}
-=======
 
->>>>>>> 1a9f9026790c8677b629a1295059e379e6505d71
+	public Bæger() {
+		Terning t1 = new Terning();
+		Terning t2 = new Terning();
+
+	}
+
+	public int bæger(String Bruger) {
+		return t1 + t2;
+
+	}
+
 }
