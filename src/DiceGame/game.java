8e4adaeeb221097f550395f9bb3322e1aa95c07f
DenBedste <32563463+DenBedste@users.noreply.github.com>
2017-10-13 14:56:02 +0200

    Update game.java

--- src/DiceGame/game.java
+++ src/DiceGame/game.java
@@ -18,7 +18,9 @@ public class game {
 	public static void main(String[] args) {
 		String Name1, Name2;
 		Scanner indput = new Scanner(System.in);
+		System.out.println("Enter name for player 1");
 		Name1 = indput.next();
+		System.out.println("Enter name for player 2");
 		Name2 = indput.next();
 		User p1 = new User(Name1);
 		User p2 = new User(Name2);
